a1387846fbda5dbf50afafc756b6a2ef965a101b
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-11-29 12:49:31 -0600

    finishing the sidebar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import {Routes, Route} from 'react-router';
+import React from "react";
+import { Routes, Route } from "react-router";
 //components
-import Ordenes from './components/pages/Ordenes';
-import Menu from './components/pages/Menu';
-import NuevoPlatillo from './components/pages/NuevoPlatillo';
-import Sidebar from './components/ui/Sidebar';
+import Ordenes from "./components/pages/Ordenes";
+import Menu from "./components/pages/Menu";
+import NuevoPlatillo from "./components/pages/NuevoPlatillo";
+import Sidebar from "./components/ui/Sidebar";
 
 function App() {
   return (
-      <div>
-        <Sidebar  />
+    <div className="md:flex min-h-screen">
+      <Sidebar />
+      <div className="md:w-3/5 xl:4/5">
         <Routes>
           <Route path="/" element={<Ordenes />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/nuevo-platillo" element={<NuevoPlatillo />} />
         </Routes>
       </div>
+    </div>
   );
 }
 
--- src/components/ui/Sidebar.js
+++ src/components/ui/Sidebar.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { NavLink} from 'react-router-dom';
 
 function Sidebar() {
     return (
         <div className="md:w-2/5 xl:w-1/5 bg-gray-800">
             <div className="p-6">
                 <p className="uppercase text-white text-2xl tracking-wide text-center font-bold">Restaurant App</p>
+                <p className="mt-3 text-gray-600">Administra tu Restaurant en las siguientes opciones:</p>
+                <nav className="mt-10">
+                    <NavLink className="p1 text-gray-600 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" exact to="/">Ordenes</NavLink>
+                    <NavLink className="p1 text-gray-600 block hover:bg-yellow-500 hover:text-gray-900" activeClassName="text-yellow-500" exact to="/menu">Menu</NavLink>
+                </nav>
             </div>
         </div>
     )
