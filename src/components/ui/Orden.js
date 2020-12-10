1836043d7280354ed5b46813be2c7e75fadc9ef9
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-12-09 23:47:28 -0600

    define time for order

--- src/components/ui/Orden.js
+++ src/components/ui/Orden.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { FirebaseContext } from "../../firebase";
 
 const Orden = ({ orden }) => {
+    const { firebase } = useContext(FirebaseContext);
+    //State
+    const [tiempoentrega, setTiempoEntrega] = useState("");
+
+    //Define el tiempo de entrega en tiempo real
+    const definirTiempo = (id) => {
+        try {
+            firebase.db.collection('ordenes').doc(id).update({tiempoentrega});
+        } catch (error) {
+            console.log("Error definir tiempo: ",error);
+        }
+    }
+
+    if(!orden) return null;
+
   return (
     <div className="sm:w-1/2 lg:w-1/3 px-2 mb-4">
       <div className="p-3 shadow-md bg-white">
         <h1 className="text-yellow-600 text-lg font-bold">{orden.id}</h1>
         {orden.orden.map((platillos, i) => (
-          <p key={platillos.id + i} className="text-gray-600">
+          <p key={ i} className="text-gray-600">
             {platillos.cantidad} {platillos.nombre}
           </p>
         ))}
@@ -16,7 +32,7 @@ const Orden = ({ orden }) => {
 
         {orden.tiempoentrega === 0 && (
           <div>
-            <label htmlFor="" className="block text-gray-700 font-bold mb-2">
+            <label  className="block text-gray-700 font-bold mb-2">
               Tiempo de Entrega
             </label>
             <input
@@ -25,10 +41,14 @@ const Orden = ({ orden }) => {
               min="1"
               max="20"
               placeholder="20"
+              name="tiempoentrega"
+              value={tiempoentrega}
+              onChange={e => setTiempoEntrega(parseInt(e.target.value))}
             />
             <button
               type="submit"
               className="bg-gray-900 hover:bg-gray-900 w-full mt-5 p-2 text-white uppercase font-bold"
+              onClick={() => definirTiempo(orden.id)}
             >
               Definir Tiempo
             </button>
