d70742351cef2f4cd46d8ffbf5929a20037743e1
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-12-10 00:54:01 -0600

    some validations

--- src/components/ui/Orden.js
+++ src/components/ui/Orden.js
@@ -2,27 +2,35 @@ import React, { useState, useEffect, useContext } from "react";
 import { FirebaseContext } from "../../firebase";
 
 const Orden = ({ orden }) => {
-    const { firebase } = useContext(FirebaseContext);
-    //State
-    const [tiempoentrega, setTiempoEntrega] = useState("");
+  const { firebase } = useContext(FirebaseContext);
+  //State
+  const [tiempoentrega, setTiempoEntrega] = useState("");
 
-    //Define el tiempo de entrega en tiempo real
-    const definirTiempo = (id) => {
-        try {
-            firebase.db.collection('ordenes').doc(id).update({tiempoentrega});
-        } catch (error) {
-            console.log("Error definir tiempo: ",error);
-        }
+  //Define el tiempo de entrega en tiempo real
+  const definirTiempo = (id) => {
+    try {
+      firebase.db.collection("ordenes").doc(id).update({ tiempoentrega });
+    } catch (error) {
+      console.log("Error definir tiempo: ", error);
     }
+  };
 
-    if(!orden) return null;
+  const completarOrden = id => {
+      try {
+          firebase.db.collection('ordenes').doc(id).update({completado: true})
+      } catch (error) {
+          console.log(error);
+      }
+  }
+
+  if (!orden) return null;
 
   return (
     <div className="sm:w-1/2 lg:w-1/3 px-2 mb-4">
       <div className="p-3 shadow-md bg-white">
         <h1 className="text-yellow-600 text-lg font-bold">{orden.id}</h1>
         {orden.orden.map((platillos, i) => (
-          <p key={ i} className="text-gray-600">
+          <p key={i} className="text-gray-600">
             {platillos.cantidad} {platillos.nombre}
           </p>
         ))}
@@ -32,7 +40,7 @@ const Orden = ({ orden }) => {
 
         {orden.tiempoentrega === 0 && (
           <div>
-            <label  className="block text-gray-700 font-bold mb-2">
+            <label className="block text-gray-700 font-bold mb-2">
               Tiempo de Entrega
             </label>
             <input
@@ -43,7 +51,7 @@ const Orden = ({ orden }) => {
               placeholder="20"
               name="tiempoentrega"
               value={tiempoentrega}
-              onChange={e => setTiempoEntrega(parseInt(e.target.value))}
+              onChange={(e) => setTiempoEntrega(parseInt(e.target.value))}
             />
             <button
               type="submit"
@@ -56,9 +64,20 @@ const Orden = ({ orden }) => {
         )}
 
         {orden.tiempoentrega > 0 && (
-            <p className="text-gray-700">Tiempo de Entrega:
-                <span className="font-bold">{orden.tiempoentrega} Minutos</span>
-            </p>
+          <p className="text-gray-700">
+            Tiempo de Entrega:
+            <span className="font-bold">{orden.tiempoentrega} Minutos</span>
+          </p>
+        )}
+
+        {!orden.completado && orden.tiempoentrega > 0 && (
+          <button
+            type="button"
+            className="bg-blue-800 hover:bg-blue-700 w-full mt-5 p-2 text-white uppercase font-bold"
+            onClick={() => completarOrden(orden.id)}
+          >
+            Marcar como lista
+          </button>
         )}
       </div>
     </div>
